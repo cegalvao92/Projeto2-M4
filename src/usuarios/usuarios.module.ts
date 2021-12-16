88926fa674c8d6b805f42d5d044314e26e64b80c
cegalvao92 <cegalvao13@gmail.com>
2021-12-15 21:05:37 -0300

    schema prisma

--- src/usuarios/usuarios.module.ts
+++ src/usuarios/usuarios.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { UsuariosService } from './usuarios.service';
 import { UsuariosController } from './usuarios.controller';
+import { Prisma } from ''
 
 @Module({
   controllers: [UsuariosController],
